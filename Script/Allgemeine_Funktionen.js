6e07e92488533c228cb6d7154e7d46d2cf4bd825
Mohamed Mahmoud Abdelwahab Abdelhakeem Elkady <mohamed.elkady@expleogroup.com>
2022-07-14 15:02:43 +0200

    commit

--- Script/Allgemeine_Funktionen.js
+++ Script/Allgemeine_Funktionen.js
@@ -1,38 +1,62 @@
 ﻿
+function Edit_Functions(Obj_Edit, Value){
+  //To solve Buffer problem of value
+  var Var1;
+  if(aqString.Contains(Value, "Buffer", 0, false)!==-1){
+    Var1 = "<Buffer"
+  }else{
+    Var1 = Value;
+  }
+  
+  //Value of actions
+  switch ( aqString.ToUpper(Var1) )
+  {
+    case "<SKIP>":
+      Skip_EditField(Obj_Edit, Value = "<SKIP>");
+      break;
+  
+    case "<CLEAR>":
+      Clear_EditField(Obj_Edit, Value = "<Clear>");
+      break;
+      
+    case "<LOG>":
+      Log_EditField(Obj_Edit, Value = "<Log>");
+      break;
+      
+    case "<BUFFER":
+      Buffer_Edit(Obj_Edit, Value);
+      break;
+      
+    default:
+      FalsherAngegebeneWert();
+  }
+}
+
+function _Test_(){
+  let EditFlied = Aliases.FlightsGUI.HwndSource_MainWindow.MainWindow.agentName;
+  Edit_Functions(EditFlied, "<BUFFER FlightNumberNEW1>")
+}
 
 function Skip_EditField(Obj_Edit, Value = "<SKIP>"){
-  if(aqString.Compare(Value, "<Skip>", false)===0){
-    Log.Message("Skip Aktion wurde ausgewählt mit " + "'" + Obj_Edit.WPFControlName +  "'" + " Object");
-  }else{
-    FalsherAngegebeneWert();
-  }
+   Log.Message("Skip Aktion wurde ausgewählt mit " + "'" + Obj_Edit.WPFControlName +  "'" + " Object"); 
 }
 
 
 function Clear_EditField(Obj_Edit, Value = "<Clear>"){
-  if(aqString.Compare(Value, "<Clear>", false)===0){
     Obj_Edit.Clear();
     Log.Message("Clear Aktion wurde ausgewählt mit " + "'" + Obj_Edit.WPFControlName +  "'" + " Object");
-  }else{
-    FalsherAngegebeneWert();
-  }
 }
 
 function Log_EditField(Obj_Edit, Value = "<Log>"){
-  if(aqString.Compare(Value, "<Log>", false)===0){
     if(aqString.Compare(Obj_Edit.wText, "", false)===0){
-      Log.Message("Der Feld "+ "'" + WPFControlName + "'" + " ist leer")
+      Log.Message("Der Feld "+ "'" + Obj_Edit.WPFControlName + "'" + " ist leer")
     }else{
       Log.Message("Der Feld enthält den Wert: " + Obj_Edit.wText);
     }
     Log.Message("Log Aktion wurde ausgewählt mit " + "'" + Obj_Edit.WPFControlName +  "'" + " Object");
-  }else{
-    FalsherAngegebeneWert();
-  }
 }
 
 function Buffer_Edit(Obj_Edit, Value = "<BUFFER FlightNumber>"){
-  if(aqString.Contains(Value, "Buffer", 0, false)!==-1){
     Value;
     // To get variable  name
     aqString.ListSeparator = " ";
@@ -49,21 +73,12 @@ function Buffer_Edit(Obj_Edit, Value = "<BUFFER FlightNumber>"){
       Log.Error("Variable wurde NICHT erstellt Oder der Wert ist nicht korrekt");
     }
     Log.Message("Buffer Aktion wurde ausgewählt mit " + "'" + Obj_Edit.WPFControlName +  "'" + " Object");
-  }else{
-    FalsherAngegebeneWert();
-  }
 }
 
 function FalsherAngegebeneWert(){
   Log.Warning("Der angegebene Wert ist nicht korrekt")
 }
 
-function _Test_(){
-  let EditFlied = Aliases.FlightsGUI.HwndSource_MainWindow.MainWindow.agentName;
-  Project.Variables.RemoveVariable("FlightNumber");
-  Buffer_Edit(EditFlied, "<BUFFER FlightNumber>");
-
-}
 
 function _Test_2(){
   let EditFlied = Aliases.FlightsGUI.HwndSource_MainWindow.MainWindow.agentName;
@@ -72,8 +87,6 @@ function _Test_2(){
   Project.Variables.AddVariable(XVar, "String");
   Project.Variables.$set("VariableByName", XVar, "Hi");
   Log.Message(Project.Variables.VariableByName(XVar));
-  
-
 }
 
 
