590d7717e7aacaf24434370d6e854f7d0e377a3c
Mohamed Mahmoud Abdelwahab Abdelhakeem Elkady <mohamed.elkady@expleogroup.com>
2022-07-14 11:34:17 +0200

    Allgemeine Func

--- Script/Allgemeine_Funktionen.js
+++ Script/Allgemeine_Funktionen.js
@@ -1,1 +1,13 @@
 ﻿
+
+function Skip_Edit(Obj_Edit, Value = "<SKIP>"){
+  if(aqString.Compare(Value, "<Skip>", false)===0){
+    Log.Message("");
+  }else{
+    Log.Warning("Der angegebene Wert ist nicht korrekt")
+  }
+}
+
+function _Test_(){
+  
+}
